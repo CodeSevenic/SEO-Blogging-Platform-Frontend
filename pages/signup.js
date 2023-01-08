ede8bd99767e9e0e33b32171fce5366b7575944a
CodeSevenic <sevenicbookings@gmail.com>
2023-01-08 09:59:26 +0200

    PurpleWeb theme code update

---
+++ pages/signup.js
@@ -0,0 +1,11 @@
+﻿import Layout from '../components/Layout/Layout';
+
+const Signup = () => {
+  return (
+    <Layout>
+      <h2>Signup page</h2>
+    </Layout>
+  );
+};
+
+export default Signup;
