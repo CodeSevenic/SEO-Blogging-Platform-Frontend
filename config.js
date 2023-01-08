96d8470074f0c7da300712c85725c72b029e3915
CodeSevenic <sevenicbookings@gmail.com>
2023-01-08 07:58:56 +0200

    SEO Blog Code Update

---
+++ config.js
@@ -0,0 +1,4 @@
+﻿import getConfig from 'next/config';
+import { publicRuntimeConfig } from './next.config';
+
+export const API = publicRuntimeConfig.PRODUCTION ? 'http://seoblog.com' : 'http://localhost:8000';
