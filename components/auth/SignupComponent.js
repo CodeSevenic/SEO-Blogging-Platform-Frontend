b90ca5dcf4243b05e34149f1bf2206d4bbf7d3f2
CodeSevenic <sevenicbookings@gmail.com>
2023-01-08 11:08:34 +0200

    PurpleWeb theme code update

--- components/auth/SignupComponent.js
+++ components/auth/SignupComponent.js
@@ -1,7 +1,30 @@
 ﻿import React from 'react';
 
 const SignupComponent = () => {
-  return <h2>SignupComponent</h2>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Handle submit');
+  };
+  const handleChange = (e) => {
+    console.log(e.target.value);
+  };
+
+  const signupForm = () => {
+    return (
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <input
+            onChange={handleChange}
+            type="text"
+            className="form-control"
+            placeholder="Enter your name"
+          />
+        </div>
+      </form>
+    );
+  };
+
+  return <>{signupForm()}</>;
 };
 
 export default SignupComponent;
